91706fe15ee94054b674fdd463266e119ad3d8af
taoli-ax <deuta-tao@outlook.com>
2023-04-05 10:57:02 +0800

    fist SpringMVC demo

---
+++ src/main/java/com/coh/controller/CupController.java
@@ -0,0 +1,14 @@
+package com.coh.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class CupController implements Controller {
+    @Override
+    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
+        System.out.println("controller ");
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+}
