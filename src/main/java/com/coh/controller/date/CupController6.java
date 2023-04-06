05bf659de6e2881bd075b121afb952a6653860b8
taoli-ax <deuta-tao@outlook.com>
2023-04-07 00:13:04 +0800

    add method controller.date.CupController6.login

--- src/main/java/com/coh/controller/date/CupController6.java
+++ src/main/java/com/coh/controller/date/CupController6.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -13,7 +17,7 @@ import java.util.List;
 @RequestMapping("date")
 public class CupController6 {
     /**
-     * landing.jsp
+     * date.jsp
      * request.getParameter replaced by args
      * @param
      * @return
@@ -22,6 +26,16 @@ public class CupController6 {
     public String CheckDate(Date date){
         System.out.println("CupController6.CheckDate"+date);
         return "save";
+    }
+
+    @RequestMapping("servlet-api")
+    public void login(String name,HttpServletRequest request, HttpServletResponse response, HttpSession httpSession){
+        System.out.println("CupController6.login:"+name);
+        System.out.println("CupController6.login:"+request);
+        System.out.println("CupController6.login:"+response);
+        System.out.println("CupController6.login:"+httpSession);
+        System.out.println("CupController6.login:"+httpSession.getServletContext());
+
     }
 
 
